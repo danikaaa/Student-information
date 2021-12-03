0aca3a4f55b3393f0e8d97e9aa44f2803c72e766
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2021-12-04 01:10:24 +0900

    Update inquiry.php

--- inquiry.php
+++ inquiry.php
@@ -5,36 +5,34 @@
     $con = mysqli_connect("localhost","root", "wjsansrk", "students");
 
     $id =$_SESSION["userid"];
-    // $kor =$_POST["kor"];
-    // $eng=$_POST["eng"];
-    // $math=$_POST["math"];
 
     $sql = "select * from subject where id ='$id'";
     $result = mysqli_query($con, $sql);
-
     $row =mysqli_fetch_array($result);
-    $db_kor=$row["kor"];
+    
+  
+    $sql = "select id rank() over (order by math desc rank from subject";
+    $result = mysqli_query($con, $sql);
     mysqli_close($con);
-
      
 
        $_SESSION["userid"] ;
        $_SESSION["userkor"]=$row["kor"];
        $_SESSION["usereng"]=$row["eng"];
        $_SESSION["usermath"]=$row["math"];
-       $total = $_SESSION['usereng']+$_SESSION['usermath']+$_SESSION['userkor'];
-       $avg =$total/3;
+       $_SESSION["usertotal"] = $row["total"];
+       $avg =$_SESSION['usertotal']/3;
+       $_SESSION["userrank"] = $row["rank"];
 
       echo " {$_SESSION['username']}님의 성적조회" ;
-      echo "<a href='login.php'> <input type='button' value='이전페이지로' > </a> " ;
+      echo "<a href='logout.php'> <input type='button' value='로그아웃' > </a> " ;
       echo "<hr>" ;
- 
       echo " 국어 : ".$_SESSION['userkor']." <br>";
       echo " 영어 : ".$_SESSION['usereng']." <br>";
       echo " 수학 : ".$_SESSION['usermath']." <br>";
-      echo " 총점 : ".$total." <br>";
+      echo " 총점 : ".$_SESSION['usertotal']." <br>";
       echo " 평균 : ".$avg." <br>";
-      echo " 석차 : ".$allrank." <br>";
+      echo " 석차 : ".$_SESSION['userrank']." <br>";
       echo "<hr>" ;
 
     
