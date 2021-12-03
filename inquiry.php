89abad6b3152fa056b9d4ec1533a8fa1a012889f
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2021-12-03 22:31:04 +0900

    Update inquiry.php

--- inquiry.php
+++ inquiry.php
@@ -1,53 +1,45 @@
-<!-- <?php
-    session_start();
-    $con = mysqli_connect("localhost","root", "wjsansrk", "students");
-
-
-
-    if ( $_SESSION['login'] =="true") {
-        echo " {$_SESSION['id']}님은 로그-인 되셨습니다.<br> " ;
-        echo " {$_SESSION['id']}님의 시험성적은 다음과 같습니다.<br> " ;
-        echo "<hr>" ;
-        echo " 영어 : ".$_SESSION['eng']." <br>";
-        echo " 수학 : ".$_SESSION['math']." <br>";
-        $total = $_SESSION['eng']+$_SESSION['math'];
-        $avg = $total / 3;
-        echo " 총점 : ".$total." <br>";
-        echo " 평균 : ".$avg." <br>";
-        echo "<hr>" ;
-        echo "<a href='logout.php'> <input type='button' value='로그-아웃' > </a> " ;
-    }
-    else echo "<script>
-            alert('먼저 로그-인을 해야 합니다!');
-            location = 'index.html';
-        </script> ";
-?> -->
 
 <?php
 
-    // if( isset($_SESSION["userid"])) $userid = $_SESSION["userid"];
-
-    $id = $_GET["userid"];
-    $name = $_POST["username"];
-    $kor = $_POST["userkor"];
-
+    session_start();;
     $con = mysqli_connect("localhost","root", "wjsansrk", "students");
+
+    $id =$_SESSION["userid"];
+    // $kor =$_POST["kor"];
+    // $eng=$_POST["eng"];
+    // $math=$_POST["math"];
+
     $sql = "select * from subject where id ='$id'";
-    mysqli_query($con, $sql);
+    $result = mysqli_query($con, $sql);
 
+    $row =mysqli_fetch_array($result);
+    $db_kor=$row["kor"];
     mysqli_close($con);
-  
-     echo " {$_SESSION['username']}님의 성적조회" ;
-     echo "<a href='login.php'> <input type='button' value='이전페이지로' > </a> " ;
-     echo "<hr>" ;
 
-     echo " 국어 : ".$_SESSION['userkor']." <br>";
-     echo " 영어 : ".$_SESSION['usereng']." <br>";
-     echo " 수학 : ".$_SESSION['usermath']." <br>";
-     echo " 총점 : ".$total." <br>";
-     $total = $_SESSION['eng']+$_SESSION['math']+$_SESSION['kor'];
-     echo " 평균 : ".$avg." <br>";
-     $avg =$total/3;
-     echo " 석차 : ".$_SESSION['userrang']." <br>";
-     echo "<hr>" ;
+     
+
+       $_SESSION["userid"] ;
+       $_SESSION["userkor"]=$row["kor"];
+       $_SESSION["usereng"]=$row["eng"];
+       $_SESSION["usermath"]=$row["math"];
+       $total = $_SESSION['usereng']+$_SESSION['usermath']+$_SESSION['userkor'];
+       $avg =$total/3;
+
+      echo " {$_SESSION['username']}님의 성적조회" ;
+      echo "<a href='login.php'> <input type='button' value='이전페이지로' > </a> " ;
+      echo "<hr>" ;
+ 
+      echo " 국어 : ".$_SESSION['userkor']." <br>";
+      echo " 영어 : ".$_SESSION['usereng']." <br>";
+      echo " 수학 : ".$_SESSION['usermath']." <br>";
+      echo " 총점 : ".$total." <br>";
+      echo " 평균 : ".$avg." <br>";
+      echo " 석차 : ".$allrank." <br>";
+      echo "<hr>" ;
+
+    
+  
+     
+    
+
 ?>
